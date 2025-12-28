08d9f161b1a06e4f056818f34627e13f91fb2111
charles <charlez20201010@gmail.com>
2025-12-28 17:16:12 +0000

    initail commit

--- src/main/java/com/onlinebankingsystem/controller/BankController.java
+++ src/main/java/com/onlinebankingsystem/controller/BankController.java
@@ -19,7 +19,7 @@ import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 @RequestMapping("api/bank/")
-@CrossOrigin(origins = "https://bank.cloudwitches.online")
+@CrossOrigin(origins = "https://bank.onyemsbhospitals.org ")
 public class BankController {
 
 	@Autowired
